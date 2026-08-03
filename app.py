0bc262b57bfa401eaec4d67373d0a42970429e97
Jules VOEGTLE <voegtlejules@gmail.com>
2026-08-03 15:15:02 +0900

    Update app.py

--- app.py
+++ app.py
@@ -502,7 +502,7 @@ def rfteam():
         fichier_csv = st.file_uploader("Please drop the file completed", type="csv")
         st.markdown("Or use an example dataset")
         if st.button("Example"):
-            fichier_csv = pd.read_csv('./ICE/data_exemple.csv')
+            fichier_csv = './ICE/data_exemple.csv'
         
         if fichier_csv is not None:
             df_importe = pd.read_csv(fichier_csv, sep=';')
